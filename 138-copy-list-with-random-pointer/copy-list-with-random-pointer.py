134c55aae9a7dc3adc6544262501575ac5d2b3c4
Vijaylakshmi_Hunakunti <168709309+Vijaylakshmihanamreddy@users.noreply.github.com>
2026-05-07 14:31:53 +0530

    Time: 39 ms (41.56%) | Memory: 13.2 MB (49.74%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -4,31 +4,34 @@ class Solution(object):
             return None
 
         # STEP 1: Insert copied nodes in between
-        curr = head
-        while curr:
-            new = Node(curr.val)
-            new.next = curr.next
-            curr.next = new
-            curr = new.next
-
-        # STEP 2: Assign random pointers
-        curr = head
-        while curr:
-            if curr.random:
-                curr.next.random = curr.random.next
-            curr = curr.next.next
-
-        # STEP 3: Separate original and copied list
-        curr = head
-        copy_head = head.next
-        copy = copy_head
-
-        while curr:
-            curr.next = curr.next.next
-            if copy.next:
-                copy.next = copy.next.next
-            curr = curr.next
-            copy = copy.next
-
-        return copy_head
+        c = head
+        while c:
+            new = Node(c.val)
+            new.next = c.next
+            c.next = new
+            c = new.next
         
+        c =  head
+        while c:
+            if c.random:
+                c.next.random = c.random.next
+            c = c.next.next
+        
+        # create a head for clone
+        clo_head = head.next
+
+        c = head
+        c1 = clo_head
+
+        while c:
+            c.next = c.next.next
+            if c1.next:
+                c1.next =c1.next.next
+            c = c.next
+            c1 = c1.next
+        return clo_head
+
+
+
+
+      
